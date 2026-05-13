c547eb91fd6dcab85f40b8f043b2ffaa2607cf02
Loewenberg, Gidon <lgidon@gmail.com>
2026-05-13 21:36:53 +0300

    Files linted

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
 import requests
 import time
-import os
 
 TFL_API_URL = "https://api.tfl.gov.uk/Line/mode/tube/Status"
+
 
 def fetch_tube_status():
     response = requests.get(TFL_API_URL)
     if response.status_code == 200:
         data = response.json()
         for line in data:
-            name = line['name']
-            status = line['lineStatuses'][0]['statusSeverityDescription']
+            name = line["name"]
+            status = line["lineStatuses"][0]["statusSeverityDescription"]
             print(f"Line: {name} | Status: {status}")
     else:
         print(f"Error fetching data: {response.status_code}")
 
+
 if __name__ == "__main__":
     while True:
         fetch_tube_status()
-        time.sleep(60) # Poll every minute
+        time.sleep(60)  # Poll every minute
